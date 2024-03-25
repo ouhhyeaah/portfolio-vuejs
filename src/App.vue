28b46508d917fe3efb96ff2aed5cd99828b90242
Louis Petitperrin <louis.petitperrin@etu.univ-grenoble-alpes.fr>
2024-03-25 13:58:33 -0400

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="customcc"></div>
+  <NavBar></NavBar>
+  <ModalView title="Le Titre"></ModalView>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
+import ModalView from "@/components/ModalView.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ModalView,
+    NavBar
   }
 }
+document.addEventListener('DOMContentLoaded', function () {
+  const cursor = document.getElementById('customcc');
+  function moveCursor(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const cursorHalfWidth = cursor.offsetWidth / 2;
+    const cursorHalfHeight = cursor.offsetHeight / 2;
+    // Smooth transition
+    cursor.style.transform = 'translate(' + (mouseX - cursorHalfWidth) + 'px, ' + (mouseY - cursorHalfHeight) + 'px)';
+  }
+  document.addEventListener('mousemove', function (e) {
+    requestAnimationFrame(function () {
+      moveCursor(e);
+    });
+  });
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  cursor: none;
 }
+#customcc{
+  width: 20px;
+  height: 20px;
+  background-color: transparent; /* Mettre le fond du curseur transparent */
+  border: 2px solid #434343; /* Ajouter une bordure rouge pour le contour */
+  border-radius: 50%; /* pour rendre le curseur rond */
+  position: fixed;
+  z-index: 9999; /* assure que le curseur soit au dessus de tout le reste */
+  pointer-events: none; /* assure que le curseur n'interfère pas avec les éléments de la page */
+}
+
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-full flex justify-between p-10">
+      <div class="text-3xl font-bold hover:shadow-2xl rounded-3xl p-2.5 transition duration-300 ease-in-out hover:scale-105">
+        Louis.
+      </div>
+      <div>
+        <ul class="flex gap-6 font-mono">
+          <li class="p-2.5 hover:shadow-xl rounded-xl px-6 transition duration-300 ease-in-out hover:scale-105" >Work</li>
+          <li class="p-2.5 hover:shadow-xl rounded-xl px-6 transition duration-300 ease-in-out hover:scale-105">About</li>
+          <li class="p-2.5 hover:shadow-xl rounded-xl px-6 transition duration-300 ease-in-out hover:scale-105">Blog</li>
+          <li class="p-2.5 hover:shadow-xl rounded-xl px-6 transition duration-300 ease-in-out hover:scale-105">Resume</li>
+          <li class="p-2.5 hover:shadow-xl rounded-xl px-6 transition duration-300 ease-in-out hover:scale-105">Contact</li>
+          <li class="p-2.5">
+            <SunIcon class="h-6 w-6 text-black-500" />
+          </li>
+        </ul>
+
+      </div>
+    </div>
+
+</template>
+<script setup>
+import { SunIcon } from '@heroicons/vue/24/solid'
+
+</script>
+
+<script>
+export default {
+  name:"NavBar",
+}
+</script>
